1eb1188acbc02d45ba0cf9b56c10613766c45209
marc.smith <theonlymarcsmith@gmail.com>
2019-05-09 11:54:54 -0400

    working on permissions.py

---
+++ Auction_API/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework import permissions
+
+class
+#Need to check if logged in user is the owner of the auction
+https://stackoverflow.com/questions/43064417/whats-the-differences-between-has-object-permission-and-has-permission-in-drfp
+https://stackoverflow.com/questions/38718454/django-rest-framework-owner-permissions
